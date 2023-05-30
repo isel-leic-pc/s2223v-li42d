2b2dcab8341c45ee8d40a24cd64c130d0cc77fc7
João Trindade <joao.trindade@isel.pt>
2023-05-30 16:35:14 +0100

    Lecture 36 (another fix)

--- 36-structured-concurrency-ii/02-semaphore/prog/src/main/kotlin/SuspendableCancellableCoroutineWithTimeout.kt
+++ 36-structured-concurrency-ii/02-semaphore/prog/src/main/kotlin/SuspendableCancellableCoroutineWithTimeout.kt
@@ -1,18 +1,24 @@
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.withTimeout
+import kotlin.math.max
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.nanoseconds
+
+// A timeout of 0 is not supported as it changes the behaviour
+// observed in suspendCancellableCoroutine, that only reacts
+// to cancellation after running 'block'.
 
 suspend inline fun <T> suspendCancellableCoroutineWithTimeout(
 	timeoutMillis: Long,
 	crossinline block: (CancellableContinuation<T>) -> Unit
-) = withTimeout(timeoutMillis) {
+) = withTimeout(max(1, timeoutMillis)) {
 	suspendCancellableCoroutine(block = block)
 }
 
 suspend inline fun <T> suspendCancellableCoroutineWithTimeout(
 	timeout: Duration,
 	crossinline block: (CancellableContinuation<T>) -> Unit
-) = withTimeout(timeout) {
+) = withTimeout(timeout.coerceAtLeast(1.nanoseconds)) {
 	suspendCancellableCoroutine(block = block)
 }
